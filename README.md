763266b0224e71f4c76b8de6b31f8eb92576a74a
JCDorne <jcdoom@hotmail.fr>
2018-10-30 09:37:09 +0100

    Add debuglib

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ACS-Stuff
+
+Some ACS files I wrote for my fctlib.
+
+## Featured :
+
+* debuglib
+
+## License
+
+This project is licensed under the GNU General Public License v3.0 - see the [LICENSE.md](LICENSE.md) file for details
---
+++ src/debuglib.h
@@ -0,0 +1,110 @@
+/*
+================================================================================
+Frenchies' Chaotic Tuesdays
+Debug Library (ACS version, Zdaemon)
+
+Version 1.0
+
+By JCD
+================================================================================
+*/
+
+
+#define	INFO	0
+#define WARNING 1
+#define ERROR	2
+
+str lvl_error[3] =
+{
+	"\cf[INFO]\c- ",		// Gold
+	"\ci[WARNING]\c- ",		// Orange
+	"\cg[ERROR]\c- "		// Red
+};
+
+
+
+//==============================================================================
+//
+// Simple Debug Message
+//
+//==============================================================================
+function void debug (int lvlerror, str msg)
+{
+	if (!GetCvar("sv_debug"))
+		return;
+	
+	Log(s:lvl_error[lvlerror], s:" - ", s:msg);
+}
+
+
+//==============================================================================
+//
+// Debug int
+//
+//==============================================================================
+function void debug_int (int lvlerror, str msg, int valeur)
+{
+	if (!GetCvar("sv_debug"))
+		return;
+	
+	Log(s:lvl_error[lvlerror], s:" - ", s:msg, s:" : ", d:valeur);
+}
+
+
+//==============================================================================
+//
+// Debug Fixed
+//
+//==============================================================================
+function void debug_fixed (int lvlerror, str msg, int valeur)
+{
+	if (!GetCvar("sv_debug"))
+		return;
+	
+	Log(s:lvl_error[lvlerror], s:" - ", s:msg, s:" : ", f:valeur);
+}
+
+
+//==============================================================================
+//
+// Debug Char
+//
+//==============================================================================
+function void debug_char (int lvlerror, str msg, int valeur)
+{
+	if (!GetCvar("sv_debug"))
+		return;
+	
+	Log(s:lvl_error[lvlerror], s:" - ", s:msg, s:" : ", c:valeur);
+}
+
+
+//==============================================================================
+//
+// Debug Bool
+//
+//==============================================================================
+function void debug_bool (int lvlerror, str msg, int valeur)
+{
+	if (!GetCvar("sv_debug"))
+		return;
+
+	if (valeur > 0)
+		Log(s:lvl_error[lvlerror], s:" - ", s:msg, s:" : True");
+	else
+		Log(s:lvl_error[lvlerror], s:" - ", s:msg, s:" : False");
+}
+
+
+//==============================================================================
+//
+// Debug String
+//
+//==============================================================================
+function void debug_str (int lvlerror, str msg, str valeur)
+{
+	if (!GetCvar("sv_debug"))
+		return;
+	
+	Log(s:lvl_error[lvlerror], s:" - ", s:msg, s:" : ", s:valeur);
+}
